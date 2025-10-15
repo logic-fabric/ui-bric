b46d212ac20341f945bac7f23fd843111f2d76b1
logic-fabric <lowic@logic-fabric.fr>
2025-10-15 10:47:00 +0200

    feat(*): example of custom Select with React Aria

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,25 @@
-import { useState } from 'react'
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button, Label, ListBox, ListBoxItem, Popover, Select, SelectValue } from 'react-aria-components'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+    <Select>
+      <Label>Animaux favoris</Label>
 
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Button>
+        <SelectValue />
+        <span aria-hidden="true">▼</span>
+      </Button>
 
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Popover>
+        <ListBox>
+          <ListBoxItem>Chat</ListBoxItem>
+          <ListBoxItem>Chien</ListBoxItem>
+          <ListBoxItem>Kangourou</ListBoxItem>
+        </ListBox>
+      </Popover>
+    </Select>
   )
 }
 
